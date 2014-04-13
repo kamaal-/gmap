b139acb8549fd56dd015abcd377f8f42c8b21146
Kamaal Aboothalib <kamaal@kamaal.me>
2014-04-13 21:07:07 +0400

    last

--- src/js/models/marker.model.js
+++ src/js/models/marker.model.js
@@ -2,16 +2,16 @@ App.Models.Marker = Backbone.Model.extend({
 
 	initialize : function(){
 
-		console.log("Marker created");
+		console.log( this );
 
 	},
 
 	defaults : {
 		lat : '0.1',
 		lng : '0.2',
-		type : 'normal' // svg, image
-		color : false // svg 
-		 
+		type : 'normal', // svg, image
+		color : false,// svg 
+            content : '<div class="wrapper"><h3>Hello</h3></div>'
 	}
 
 });
